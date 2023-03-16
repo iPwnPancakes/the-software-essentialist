725ada3540d8dacff4559bb924be971f31fead21
Joost van Driel <joostvdriel@gmail.com>
2023-03-16 16:10:24 +0100

    Parameterized the test

--- 1 The Physical/Guess Point Level 1: Stateless/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1: Stateless/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -1,23 +1,18 @@
 import {isPalindrome} from "./index";
 
 describe('palindrome checker', () => {
-    it('should return true for mom', () => {
-        expect(isPalindrome('mom')).toBe(true)
-    })
+    const palindromes = [
+        'mom',
+        'wow',
+        'WoW',
+        'Was It A Rat I Saw'
+    ]
 
-    it('should return true for wow', () => {
-        expect(isPalindrome('wow')).toBe(true)
-    })
-
-    it('should return true for WoW', () => {
-        expect(isPalindrome('WoW')).toBe(true)
+    it.each(palindromes)('should return true for palindrome "%i"', (palindrome) => {
+        expect(isPalindrome(palindrome)).toBe(true)
     })
 
     it('should return false for bill', () => {
         expect(isPalindrome('bill')).toBe(false)
-    })
-
-    it('should return true for Was It A Rat I Saw', () => {
-        expect(isPalindrome('Was It A Rat I Saw')).toBe(true)
     })
 })
