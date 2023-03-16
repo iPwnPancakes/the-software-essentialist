d8fe5d19e9a644d1daebee5175b79d6076bbfec3
Joost van Driel <joostvdriel@gmail.com>
2023-03-16 16:35:23 +0100

    Created validator

--- 1 The Physical/Guess Point Level 1: Stateless/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1: Stateless/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import {validatePassword} from "./index";
 
 describe('password validator', () => {
-
+    it('should return false for empty password', () => {
+        expect(validatePassword('')).toBe(false)
+    })
 })
 
